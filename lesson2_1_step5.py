0208828202888e6991f42fea249b9b94da6777b0
yuri1985fedotov <neoplan18785@mail.ru>
2025-09-22 11:19:32 +0300

    Кликаем по checkboxes и radiobuttons (капча для роботов)

---
+++ lesson2_1_step5.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    sunduk = browser.find_element(By.TAG_NAME, "img")
+    people_checked = sunduk.get_attribute("valuex")
+
+    # Ваш код, который заполняет обязательные поля
+    x = people_checked
+    y = calc(x)
+
+    # Отправляем заполненную форму
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+    option3 = browser.find_element(By.CLASS_NAME, "btn.btn-default")
+    option3.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(5)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
