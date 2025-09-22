fdfccd3726c443e9abc4a4d0941333cf10f05cfc
yuri1985fedotov <neoplan18785@mail.ru>
2025-09-22 11:34:07 +0300

    Переход на новую вкладку

---
+++ lesson2_3_step6.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try: 
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    option1 = browser.find_element(By.CLASS_NAME, "trollface.btn.btn-primary")
+    option1.click()
+    
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    
+    option3 = browser.find_element(By.CLASS_NAME, "btn.btn-primary")
+    option3.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(5)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
