a9d267faf1959cd9ddca20634ed03704dbb69031
yuri1985fedotov <neoplan18785@mail.ru>
2025-09-22 11:38:17 +0300

    Selenium Waits(Implicit Waits)

---
+++ lesson2_4_step5.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(5)
+browser.get("http://suninjuly.github.io/wait1.html")
+
+button = browser.find_element(By.ID, "verify")
+button.click()
+message = browser.find_element(By.ID, "verify_message")
+
+assert "successful" in message.text
